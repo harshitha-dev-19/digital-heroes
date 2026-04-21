02510d8f6e8bec57b96d1dc7de8529012f98f39a
harshi <harshithapn19@gmail.com>
2026-04-21 22:45:31 +0530

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-<< END
-from django.contrib.auth import get_user_model
-User = get_user_model()
+python manage.py shell -c "
+from django.contrib.auth.models import User
 if not User.objects.filter(username='harshi').exists():
     User.objects.create_superuser('harshi', 'channelwebdev@gmail.com', 'QWERTY@098')
-END
+"
