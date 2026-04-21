b67171f5f10f0a486c89b8ce6b2813a436ebf37f
harshi <harshithapn19@gmail.com>
2026-04-21 23:13:26 +0530

    fix super user

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ python manage.py shell -c "
 from django.contrib.auth.models import User
 if not User.objects.filter(username='harshi').exists():
     User.objects.create_superuser('harshi', 'channelwebdev@gmail.com', 'QWERTY@098')
+print('Superuser ready')
 "
